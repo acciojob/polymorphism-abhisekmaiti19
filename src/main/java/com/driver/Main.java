489d0b99487822482d13745106460259ab2cc999
Abhisek Maiti <abhisekmaiti19@gmail.com>
2022-10-07 21:34:52 +0530

    updation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -11,7 +11,7 @@ public class Main {
         Product p = new Product();
         int ans = p.product(10,15);
         int ans1 = p.product(10,15,20);
-        int ans2 = p.product(10.02,10.6);
+        double ans2 = p.product(10.02,10.6);
         System.out.println(ans+" "+ans1+" "+ans2);
     }
     
